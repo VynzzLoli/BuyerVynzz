5462aac7d6bb98f4f453b77705a9770b7ee43a97
Vynzz Loli <97965697+VynzzLoli@users.noreply.github.com>
2022-02-07 12:58:42 +0700

    Update sewa.js
    P

--- ryudata/sewa.js
+++ ryudata/sewa.js
@@ -1,10 +1,10 @@
 exports.sewa = () => {
 	return`𝗢 𝗣 𝗘 𝗡  𝗦 𝗘 𝗪 𝗔  𝗕 𝗢 𝗧
 
-• *1 Minggu : 5.000 ( 5K )*
-• *1 Bulan : 10.000 ( 10K )*
-• *Permanen : 15.000 ( 15K )*
-• *Join Murid Bot : 20.000 ( 20K )*
+• *1 Minggu : 4.000 ( 4K )*
+• *1 Bulan : 7.000 ( 7K )*
+• *Permanen : 10.000 ( 10K )*
+• *Join Murid Bot : 15.000 ( 15K )*
 
 Minat Ketik .owner
 `
